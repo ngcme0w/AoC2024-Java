8dfcb79fe052f46ee2b71f3db327e45e74588de6
ngcme0w <177364454+ngcme0w@users.noreply.github.com>
2024-12-08 14:09:07 +0000

    Removed unnecessary import

--- src/main/java/meow/ngc/Main.java
+++ src/main/java/meow/ngc/Main.java
@@ -2,8 +2,6 @@ package meow.ngc;
 
 import meow.ngc.Day.One;
 
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
         final One dayOne = new One();
