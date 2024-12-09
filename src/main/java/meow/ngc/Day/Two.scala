07a42f3f90635b961b0c6c8398ce2b87044f3df3
ngcme0w <177364454+ngcme0w@users.noreply.github.com>
2024-12-09 00:54:39 +0000

    Added day two part one

---
+++ src/main/java/meow/ngc/Day/Two.scala
@@ -0,0 +1,31 @@
+package meow.ngc.Day
+
+import meow.ngc.InputHandler
+
+import scala.util.Properties
+
+class Two extends InputHandler {
+  private val inputLocation: String = "src/inputs/Two/input"
+
+  private val inputsAsList: List[Array[Int]] =
+    getInputs(inputLocation).split(Properties.lineSeparator)
+                 .map(line => line
+                   .split(" ")
+                   .map(_.toInt)).toList
+
+  private def isInConstraints(report: Array[Int]): Boolean = {
+    val isIncreasing = report.sliding(2).forall {
+      case Array(a, b) => (b - a >= 1) && (b - a <= 3)
+    }
+
+    val isDecreasing = report.sliding(2).forall {
+      case Array(a, b) => (a - b >= 1) && (a - b <= 3)
+    }
+
+    isIncreasing || isDecreasing
+  }
+
+  def getNumberOfSafeReports: Int = {
+    inputsAsList.count(report => isInConstraints(report))
+  }
+}
--- src/main/java/meow/ngc/Main.java
+++ src/main/java/meow/ngc/Main.java
@@ -1,11 +1,17 @@
 package meow.ngc;
 
 import meow.ngc.Day.One;
+import meow.ngc.Day.Two;
 
 public class Main {
     public static void main(String[] args) {
         final One dayOne = new One();
         System.out.println("Day one answer: " + dayOne.getPairsDistanceSum());
         System.out.println("Total similarity score: " + dayOne.calculateSimilarity());
+        System.out.println();
+
+        final Two dayTwo = new Two();
+        System.out.println("Day two Answer: " + dayTwo.getNumberOfSafeReports());
+        System.out.println();
     }
 }
