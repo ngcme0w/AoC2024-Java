a8d8fd8e7d97667dbc7a7b9b25a3b9be95c62ea7
ngcme0w <177364454+ngcme0w@users.noreply.github.com>
2024-12-10 16:32:39 +0000

    Solved day 3 part 1

--- src/main/java/meow/ngc/Day/Three.java
+++ src/main/java/meow/ngc/Day/Three.java
@@ -1,7 +1,30 @@
 package meow.ngc.Day;
 
 import meow.ngc.InputHandler;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Three extends InputHandler {
+    private final String inputLocation = "src/inputs/Three/input";
 
+    private int calculateMultiplesSum() {
+        int product = 0;
+
+        if (!getInputs(inputLocation).isBlank()) {
+            final String inputs = getInputs(inputLocation);
+            final Pattern desiredExpr = Pattern.compile("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)");
+
+            Matcher m = desiredExpr.matcher(inputs);
+            while (m.find()) {
+                System.out.println(Integer.valueOf(m.group(1)) + " + " + Integer.valueOf(m.group(2)));
+                product += (Integer.valueOf(m.group(1)) * Integer.valueOf(m.group(2)));
+            }
+        }
+
+        return product;
+    }
+
+    public final int runInputsCalculation() {
+        return calculateMultiplesSum();
+    }
 }
--- src/main/java/meow/ngc/Main.java
+++ src/main/java/meow/ngc/Main.java
@@ -2,16 +2,18 @@ package meow.ngc;
 
 import meow.ngc.Day.One;
 import meow.ngc.Day.Two;
+import meow.ngc.Day.Three;
 
 public class Main {
     public static void main(String[] args) {
         final One dayOne = new One();
         System.out.println("Day one answer: " + dayOne.getPairsDistanceSum());
         System.out.println("Total similarity score: " + dayOne.calculateSimilarity());
-        System.out.println();
 
         final Two dayTwo = new Two();
-        System.out.println("Day two Answer: " + dayTwo.getNumberOfSafeReports());
-        System.out.println();
+        System.out.println("Day two answer: " + dayTwo.getNumberOfSafeReports());
+
+        final Three dayThree = new Three();
+        System.out.println("Day three answer: " + dayThree.runInputsCalculation());
     }
 }
